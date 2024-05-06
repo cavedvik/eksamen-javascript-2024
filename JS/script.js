4ee2b38259b7da3a1a3b8c2ec0aed5674c73ac9f
cavedvik <cavedvik@gmail.com>
2024-05-06 21:42:01 +0200

    lagt til flere filer. laget POST metode hvor favoritter blir lagret i crud

--- JS/script.js
+++ JS/script.js
@@ -1,32 +1,35 @@
-
-const url = "https://pokeapi.co/api/v2/pokemon?limit=20";
-
+//Globale variabler
+const baseURL = "https://crudapi.co.uk/api/v1/pokemons";
+const pokeapiUrl = "https://pokeapi.co/api/v2/pokemon?limit=20";
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: "Bearer wYXuv1_mywtLzDEEy4tKMUmd_dvKp5gI3QsHBtKB7mooH5LPyA",
+};
 const pokemonColors = {
-  bug: "#A8B91A",        
-  dark: "#706056",       
-  dragon: "#6F45FC",     
-  electric: "#F8E02C",   
-  fairy: "#D695AD",      
-  fighting: "#D22F28",   
-  fire: "#EF8330",       
-  flying: "#B9A0F3",     
-  ghost: "#846797",      
-  grass: "#82D74C",      
-  ground: "#E3C065",     
-  ice: "#A6E9E6",        
-  normal: "#B8B77A",     
-  poison: "#B34EB1",     
-  psychic: "#FA6597",    
-  rock: "#C7B136",       
-  steel: "#C7C7DF",      
-  water: "#6490F1",      
-  
+  bug: "#A8B91A",
+  dark: "#706056",
+  dragon: "#6F45FC",
+  electric: "#F8E02C",
+  fairy: "#D695AD",
+  fighting: "#D22F28",
+  fire: "#EF8330",
+  flying: "#B9A0F3",
+  ghost: "#846797",
+  grass: "#82D74C",
+  ground: "#E3C065",
+  ice: "#A6E9E6",
+  normal: "#B8B77A",
+  poison: "#B34EB1",
+  psychic: "#FA6597",
+  rock: "#C7B136",
+  steel: "#C7C7DF",
+  water: "#6490F1",
 };
 
 //fetcher pokemons
 const pokeFetch = async () => {
   try {
-    const response = await fetch(url);
+    const response = await fetch(pokeapiUrl);
     if (!response.ok) {
       throw new Error("Error" + response.status);
     }
@@ -51,31 +54,50 @@ const fetchPokemonDetails = async (pokemonUrl) => {
     const speciesResponse = await fetch(pokemonData.species.url);
     const speciesData = await speciesResponse.json();
     pokemonItem(pokemonData, speciesData);
-    console.log(pokemonData);
+    console.log(speciesData);
   } catch (error) {
     console.error("there is a problem fetching the Pokemon details", error);
   }
 };
 
+//lagrer favoritter i crud
+const addFavouriteCrud = async (pokemon) => {
+  try {
+    const response = await fetch(baseURL, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify([pokemon]),
+    });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const responseData = await response.json();
+    console.log("Added to CRUD API", responseData);
+    return responseData;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+};
+
+//lager pokemon kortet
 const pokemonItem = (pokemonData, speciesData) => {
   const pokemonDiv = document.getElementById("pokeApi");
   const pokemonContainer = document.createElement("div");
-
-  const typeColor = pokemonColors[pokemonData.types[0].type.name] || '#ffffff'; //chat-gpt hjelp
+  const typeColor = pokemonColors[pokemonData.types[0].type.name] || "#ffffff"; //chat-gpt hjelp
 
   pokemonContainer.style.cssText = `
       background-color: ${typeColor};
       border-radius: 10px;
-      width: 300px;
+      width: 200px;
       padding: 10px;
       margin: 10px;
       `;
 
   if (pokemonData.sprites?.front_default) {
     const imageElement = document.createElement("img");
-    imageElement.style.cssText = `width: 200px; height: 200px;`;
-    imageElement.src = pokemonData.sprites.other["official-artwork"].front_shiny;
-    imageElement.loading = "lazy";
+    imageElement.style.cssText = `width: 100px; height: 100px;`;
+    imageElement.src =
+      pokemonData.sprites.other["official-artwork"].front_shiny;
     pokemonContainer.appendChild(imageElement);
   }
 
@@ -84,7 +106,9 @@ const pokemonItem = (pokemonData, speciesData) => {
   pokemonContainer.appendChild(nameElement);
 
   const typesElement = document.createElement("p");
-  typesElement.textContent = `${pokemonData.types.map(t => t.type.name).join(', ')}`;
+  typesElement.textContent = `${pokemonData.types
+    .map((t) => t.type.name)
+    .join(", ")}`;
   pokemonContainer.appendChild(typesElement);
 
   const infoBtn = document.createElement("button");
@@ -93,9 +117,19 @@ const pokemonItem = (pokemonData, speciesData) => {
 
   const addToFavorite = document.createElement("button");
   addToFavorite.innerText = "Add to Favorite";
+  addToFavorite.id = "addToFavorite";
   pokemonContainer.appendChild(addToFavorite);
 
   pokemonDiv.appendChild(pokemonContainer);
+
+  addToFavorite.addEventListener("click", async () => {
+    try {
+      const addedPokemon = await addFavouriteCrud(pokemonData);
+      console.log("Pokemon added to favorites:", addedPokemon);
+    } catch (error) {
+      console.error("Error adding Pokemon to favorites:", error);
+    }
+  });
 };
 
 const randomizer = (array) => {
@@ -107,6 +141,3 @@ const randomizer = (array) => {
 };
 
 pokeFetch();
-
-
-
