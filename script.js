2e7bdf520cef66d9e10a2a93a2df94e6a7d37b75
cavedvik <cavedvik@gmail.com>
2024-05-06 11:28:12 +0200

    laget en fetch som henter ut pokemons

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+const url = "https://pokeapi.co/api/v2/pokemon";
 
+const pokeFetch = async () => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Error" + response.status);
+        }
+        const data = await response.json();
+        console.log(data.results)
+    } catch (error) {
+        console.error("There is a problem fetching the data", error);
+    }
+};
+
+pokeFetch()
 
