af841127b60e5069df69008511c13ea9388e1a46
cavedvik <cavedvik@gmail.com>
2024-05-06 10:52:43 +0200

    laget globale variabler på crud url og headers

---
+++ script.js
@@ -0,0 +1,12 @@
+// globale varianler
+const baseURL = "https://crudapi.co.uk/api/v1";
+const headers = {
+    "Content-Type": "application/json",
+    Authorization: "Bearer -lHHnLLnpSM7jT474Sv_tD1MaGTDVdtqBUV1-GWX9QoUCFg9Hg",
+};
+
+
+
+
+
+
