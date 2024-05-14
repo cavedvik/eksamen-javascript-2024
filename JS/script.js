371b1911137e8e0a17b65f27f026da9c69a49273
cavedvik <cavedvik@gmail.com>
2024-05-14 14:04:33 +0200

    opprettet selection

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,43 @@
-import { pokeFetch, logOutUser, updateFavoritesCount, visibleProfileLink } from "./helpers.js";
+import {
+  pokeFetch,
+  logOutUser,
+  updateFavoritesCount,
+  visibleProfileLink,
+  pokeapiUrl,
+  fetchPokemonDetails,
+} from "./helpers.js";
 
-updateFavoritesCount()
-pokeFetch()
-logOutUser()
-visibleProfileLink()
+export const pokeFetchByType = async (type) => {
+  if (!type) {
+    console.error("No type selected.");
+    return;
+  }
 
+  const url = `https://pokeapi.co/api/v2/type/${type.toLowerCase()}`;
 
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    document.getElementById("pokeApi").innerHTML = "";
+    data.pokemon.forEach((p) => fetchPokemonDetails(p.pokemon.url));
+  } catch (error) {
+    console.error(`Problem fetching Pokémon by type at ${url}: `, error);
+  }
+};
 
+document
+  .getElementById("pokemonTypeSelect")
+  .addEventListener("change", function () {
+    pokeFetchByType(this.value);
+  });
 
-
-
+document.getElementById("loadMore").addEventListener("click", pokeFetch);
+//browser history push state
+pokeFetchByType();
+updateFavoritesCount();
+pokeFetch();
+logOutUser();
+visibleProfileLink();
